97731445a2ba3236200af5644304a64dc077ddba
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-01-21 09:53:30 -0500

    Updates

--- api/sendPageview.js
+++ api/sendPageview.js
@@ -1,25 +1,33 @@
 const axios = require('axios');
 const cors = require('cors');
 
-// CORS and helper function similar to submitForm.js
+const corsHandler = cors(); // Initialize CORS handler
 
 module.exports = async (req, res) => {
-  await runMiddleware(req, res, cors);
+  try {
+    console.log('Inside the serverless function...');
 
-  if (req.method === 'POST') {
-    try {
-      // Replace with your Airtable webhook URL
-      const response = await axios.post('https://hooks.airtable.com/workflows/v1/genericWebhook/apppolunZpqRjokmM/wflSvlbC4gCgQQwNI/wtrMjosf8w3JjvADn', req.body, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      res.status(200).json(response.data);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    corsHandler(req, res, async () => { // Wrap the main logic inside corsHandler
+      if (req.method === 'POST') {
+        try {
+          // Replace with your Airtable webhook URL
+          const response = await axios.post('https://hooks.airtable.com/workflows/v1/genericWebhook/apppolunZpqRjokmM/wflSvlbC4gCgQQwNI/wtrMjosf8w3JjvADn', req.body, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          res.status(200).json(response.data);
+        } catch (error) {
+          console.error('Error:', error);
+          res.status(500).json({ message: error.message });
+        }
+      } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+      }
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
